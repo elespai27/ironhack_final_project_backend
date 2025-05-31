afe51b4316ae8a3f49ea609a721afc4558b8e42b
elespai27 <elespai27@gmail.com>
2025-05-31 20:45:31 +0200

    feat(api): prepares backend for frontend integration

--- Final-Project/src/main/java/com/ironhack/Final/Project/FinalProjectApplication.java
+++ Final-Project/src/main/java/com/ironhack/Final/Project/FinalProjectApplication.java
@@ -2,9 +2,21 @@ package com.ironhack.Final.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FinalProjectApplication {
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
